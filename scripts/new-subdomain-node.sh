e1acab58cba864a05409f16f6068736fca7fc306
Atmo Suwiryo <suwiryo.atmo@gmail.com>
2024-07-27 17:12:06 +0700

    fix(template): fixing path to main.js

--- scripts/new-subdomain-node.sh
+++ scripts/new-subdomain-node.sh
@@ -33,8 +33,8 @@ fi
 CONF="$DOMAIN_DIR/domain.conf.json"
 TMP_CONF=$(mktemp)
 # Get domain from ServerName in bitnami.conf
-EMAIL=$(jq -r '.sslEmail' $CONF)
-DOMAIN=$(jq -r '.domainName' $CONF)
+EMAIL=$(jq -r '.sslEmail' "$CONF")
+DOMAIN=$(jq -r '.domainName' "$CONF")
 DOMAIN=${SUBDOMAIN}.${DOMAIN}
 
 SANITIZED_DOMAIN=${DOMAIN//./_} 
@@ -44,34 +44,34 @@ SANITIZED_DOMAIN=${DOMAIN//./_}
 
 # Add subdomain to list
 # - Remove first, so that there is no duplicate if already exist
-jq --arg subDomain $SUBDOMAIN '.subDomains |= .-[$subDomain]' $CONF > $TMP_CONF && mv $TMP_CONF $CONF
+jq --arg subDomain "$SUBDOMAIN" '.subDomains |= .-[$subDomain]' "$CONF" > "$TMP_CONF" && mv "$TMP_CONF" "$CONF"
 
 # - Add to array subdomain after that
-jq --arg subDomain $SUBDOMAIN '.subDomains |= .+[$subDomain]' $CONF > $TMP_CONF && mv $TMP_CONF $CONF
+jq --arg subDomain "$SUBDOMAIN" '.subDomains |= .+[$subDomain]' "$CONF" > "$TMP_CONF" && mv "$TMP_CONF" "$CONF"
 
 #################################################
 # Add new user
 echo '> Creating User'
-sudo useradd --system --gid webapps --shell /bin/bash --home /webapps/${SANITIZED_DOMAIN}__nodejs $SANITIZED_DOMAIN
+sudo useradd --system --gid webapps --shell /bin/bash --home /webapps/"${SANITIZED_DOMAIN}"__nodejs "$SANITIZED_DOMAIN"
 
 # Create user directory
 echo '> Creating User Directory'
-sudo mkdir -p /webapps/${SANITIZED_DOMAIN}__nodejs/
-sudo chown $SANITIZED_DOMAIN /webapps/${SANITIZED_DOMAIN}__nodejs/
+sudo mkdir -p /webapps/"${SANITIZED_DOMAIN}"__nodejs/
+sudo chown "$SANITIZED_DOMAIN" /webapps/"${SANITIZED_DOMAIN}"__nodejs/
 
 #################################################
 # Create nodejs project
 echo '> Creating Node Js Project template'
-sudo su - $SANITIZED_DOMAIN -c 'cd ~ && 
+sudo su - "$SANITIZED_DOMAIN" -c 'cd ~ && 
     mkdir -p dist &&
-    cp "$PWD/template/main.js" "dist/main.js"'
+    cp "${0}/main.js" "dist/main.js"' -- "$PWD/template"
 
 #################################################
 # Setup to serve nodejs project
 # Copy supervisor conf script
 echo '> Creating Supervisor Conf Script'
-sudo su - $SANITIZED_DOMAIN -c ' mkdir /webapps/${0}__nodejs/logs && 
-    touch /webapps/${0}__nodejs/logs/gunicorn_supervisor.log' -- $SANITIZED_DOMAIN
+sudo su - "$SANITIZED_DOMAIN" -c ' mkdir /webapps/${0}__nodejs/logs && 
+    touch /webapps/${0}__nodejs/logs/gunicorn_supervisor.log' -- "$SANITIZED_DOMAIN"
 sudo sed "s/CHANGE_HERE/$SANITIZED_DOMAIN/g" "$PWD/template/supervisor_node.conf" | sudo tee "/etc/supervisor/conf.d/${SANITIZED_DOMAIN}.conf" > /dev/null
 
 # Reread & update supervisor to start apps
@@ -85,11 +85,11 @@ echo '> Creating Nginx Entry'
 sudo sed "s/CHANGE_HERE/$SANITIZED_DOMAIN/g; s/DOMAIN_HERE/$DOMAIN/g" "$PWD/template/nginx_entry_node" | sudo tee "/etc/nginx/sites-available/$DOMAIN" > /dev/null
 
 # Enable nginx entry
-sudo ln -s /etc/nginx/sites-available/$DOMAIN /etc/nginx/sites-enabled/$DOMAIN
+sudo ln -s /etc/nginx/sites-available/"$DOMAIN" /etc/nginx/sites-enabled/"$DOMAIN"
 
 # Add let's encrypt https
 echo '> Creating Lets Encrypt certificate'
-sudo certbot --nginx -m $EMAIL --agree-tos --redirect -d $DOMAIN -d www.$DOMAIN 
+sudo certbot --nginx -m "$EMAIL" --agree-tos --redirect -d "$DOMAIN" -d www."$DOMAIN" 
 
 # Restart nginx
 echo '> Restarting nginx'
